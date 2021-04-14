f64b5ccb7c26b25d76fb534362da570091af2a2a
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2021-04-15 10:12:03 +1200

    Created player and element base code

---
+++ Elements/Element.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RogueLike.Elements
+{
+    class Element
+    {
+        public string Name { get; set; }
+        public char Icon { get; set; }
+
+        public Element(string name, char icon)
+        {
+            Name = name;
+            Icon = icon;
+        }
+    }
+}
---
+++ Elements/Entities/PlayerBody.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RogueLike.Elements
+{
+    class Player : Element
+    {
+        public Player(string name, char icon) : base(name, icon)
+        {
+        }
+
+        public int Level { get; set; }
+        public int Strength { get; set; }
+    }
+}
