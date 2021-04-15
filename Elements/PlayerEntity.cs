f9bbe329c30a4ac9187c47b118e67891e46e933e
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2021-04-15 14:58:03 +1200

    Created structures and put them all inside just one folder

---
+++ Elements/PlayerEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RogueLike.Elements
+{
+    class PlayerBody : Element
+    {
+        public int Level { get; set; }
+        public int Strength { get; set; }
+
+        public PlayerBody(string name, char icon, int level, int strength) : base(name, icon)
+        {
+            Level = level;
+            Strength = strength;
+        }
+
+        public int Movement(char key)
+        {
+            return key switch
+            {
+                'w' => 0,
+                's' => 1,
+                'a' => 2,
+                'd' => 3,
+                _ => -1,
+            };
+        }
+    }
+}
---
+++ Elements/Structure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RogueLike.Elements.Structures
+{
+    class Structure : Element
+    {
+        public bool Solid { get; set; }
+        public int Hardness { get; set; }
+
+        public Structure(string name, char icon, bool solid, int hardness) : base(name, icon)
+        {
+            Solid = solid;
+            Hardness = hardness;
+        }
+    }
+}
